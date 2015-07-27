0b289e4d83370fd2f550afa16f8d509def934438
ownport <ownport@gmail.com>
2015-07-27 10:04:46 +0300

    [+] added plugins and static files

--- engine/pelicanconf.py
+++ engine/pelicanconf.py
@@ -6,34 +6,71 @@ from __future__ import unicode_literals
 AUTHOR = u'ownport'
 SITENAME = u'ownport.github.io/notes'
 SITEURL = u'http://ownport.github.io/notes'
-
-PATH = u'content'
-# Static paths
-STATIC_PATHS = ['images', 'pdfs']
-
 TIMEZONE = u'Europe/Kiev'
-
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
+# -----------------------------------
+#   Paths
+# -----------------------------------
+
+PATH = u'content'
+STATIC_PATHS = [
+    # Directories
+    'images', 'pdfs',
+    # Files 
+    'static/robots.txt',
+    'static/googleec9d63c38ebcca03.html',
+]
+
+# path-specific metadata
+EXTRA_PATH_METADATA = {
+    'static/robots.txt': {'path': 'robots.txt'},
+    'static/googleec9d63c38ebcca03.html': {'path': 'googleec9d63c38ebcca03.html'},
+}
+
+# -----------------------------------
+#   Feeds
+# -----------------------------------
+
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('GitHub', 'http://github.com/ownport'),)
+# -----------------------------------
+#   Links
+# -----------------------------------
 
-# Social widget
+LINKS = (('GitHub', 'http://github.com/ownport'),)
 SOCIAL = ()
 
+# -----------------------------------
+#   Plugins
+# -----------------------------------
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sitemap',]
 
-DEFAULT_PAGINATION = 10
+#   Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
-# Theme
-
+# -----------------------------------
+#   Looks and feels
+# -----------------------------------
 THEME = 'nikhil'
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
