e4e9566ff6c842625c9d57985ededab199f4f79b
ownport <ownport@gmail.com>
2015-07-29 07:27:24 +0300

    [+] google analytics code

--- engine/pelicanconf.py
+++ engine/pelicanconf.py
@@ -76,3 +76,8 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# -----------------------------------
+#   Google
+# -----------------------------------
+GOOGLE_ANALYTICS = "UA-65731833-1"
